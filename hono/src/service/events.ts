bc684fdbae869f6a1071ceecb5bca4523211c5f0
Dylen Greenenwald <gradyl16@protonmail.com>
2024-02-10 13:29:02 -0600

    feat: event voting

--- hono/src/service/events.ts
+++ hono/src/service/events.ts
@@ -8,22 +8,18 @@ import injectDB from '../utils/injectDB'
 
 const app = new Hono<{ Bindings: Bindings; Variables: Variables }>()
 
-const insertEventSchema = createInsertSchema(events)
-
 app.get('/', injectDB, async (c) => {})
 
-app.post('/', zValidator('json', insertEventSchema), injectDB, async (c) => {
+app.post('/', zValidator('json', createInsertSchema(events).omit({ id: true })), injectDB, async (c) => {
     const body = c.req.valid('json')
-
-    // Create a new event
     await c.get('db').insert(events).values(body).returning()
     return c.text('Success')
 })
 
-// To update vote / preference on event in group
-// app.post('/', injectDB, async (c) => {
-//   const body = await c.req.json()
-//   return c.json(await c.get('db').insert(usersToGroupEvents).values(body).returning())
-// })
+app.post('/vote', zValidator('json', createInsertSchema(usersToGroupEvents)), injectDB, async (c) => {
+  const body = await c.req.json()
+  await c.get('db').insert(usersToGroupEvents).values(body).returning()
+  return c.text('Success')
+})
 
 export default app
