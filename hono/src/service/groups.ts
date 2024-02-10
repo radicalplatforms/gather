f3431e24ad8236ea3f05e98e781ed1de01989b5e
Radison Akerman <radison@me.com>
2024-02-10 03:33:35 -0600

    fix: patched nits

--- hono/src/service/groups.ts
+++ hono/src/service/groups.ts
@@ -1,14 +1,10 @@
-import { and, asc, desc, eq, like, or, sql } from 'drizzle-orm'
 import { Hono } from 'hono'
-import {} from '../schema'
 import type { Bindings, Variables } from '../utils/injectDB'
 import injectDB from '../utils/injectDB'
 
 const app = new Hono<{ Bindings: Bindings; Variables: Variables }>()
 
 app.get('/', injectDB, async (c) => {
-  const authorUsername = 'rak3rman'
-
   return c.json(
     await c.get('db').query.groups.findMany({
       with: {
